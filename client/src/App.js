57b2870a04444c394e1454ffbcad372dcd77cf4c
Mako3 <big.mako@gmail.com>
2020-10-05 20:47:54 -0700

    updated routes navbar and dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import Splash from './components/Splash'
+import React from "react";
+import Splash from "./pages/Splash";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 
-function App(){
-  return (
-    <Splash />
-  )
+function App() {
+    return (
+            <Router>
+              <Switch>
+                <Route exact path="/" component={Splash} />
+                <Route path="/dashboard" exact component={Dashboard} />
+                </Switch>
+            </Router>
+    );
 }
 
 export default App;
--- client/src/components/AceEditor/AceEditor.js
+++ client/src/components/AceEditor/AceEditor.js
@@ -1,34 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import AceEditor from 'react-ace';
-import ConsoleWrapper from '../ConsoleWrapper/consoleWrapper'
-import { console, consoleMessages } from "./consoleLogic"
+import React, { useEffect, useState } from "react";
+import AceEditor from "react-ace";
+import ConsoleWrapper from "../ConsoleWrapper/consoleWrapper";
+import { console, consoleMessages } from "../../utils/consoleLogic";
 // import editorContext from '../utils/editorContext'
 
 // required editor to work
-import 'ace-builds/src-min-noconflict/mode-html'
-import 'ace-builds/src-min-noconflict/theme-monokai'
-import 'ace-builds/src-min-noconflict/snippets/html'
-import "ace-builds/webpack-resolver"
+import "ace-builds/src-min-noconflict/mode-html";
+import "ace-builds/src-min-noconflict/theme-monokai";
+import "ace-builds/src-min-noconflict/snippets/html";
+import "ace-builds/webpack-resolver";
 
 //sets up new console
 window.console = console;
 
 function AcEditor() {
     //contexts
-    const [editor, setEditor] = useState({})
+    const [editor, setEditor] = useState({});
     // const [consoleLog, setConsoleLog] = useState([])
 
     useEffect(() => {
-        const previousCode = localStorage.getItem("code")
+        const previousCode = localStorage.getItem("code");
         if (previousCode) {
-            setEditor({ userCode: previousCode })
+            setEditor({ userCode: previousCode });
         }
-    }, [])
-    
+    }, []);
+
     //get the Ace editor value as uyer types
     function onChange(newValue) {
-        setEditor({ userCode: newValue })
-
+        setEditor({ userCode: newValue });
     }
 
     //onclick for copying and excecuting click functions
@@ -56,7 +55,6 @@ function AcEditor() {
 
     //         saveConsoleMsgs(messages);
 
-
     //     } catch (err) {
     //         console.error(err)
     //     }
@@ -70,18 +68,20 @@ function AcEditor() {
     //     saveConsoleMsgs([]);
     // }
 
-
-
-
     return (
         <div className="AceEditor">
-
             <AceEditor
-                mode="javascript" theme="monokai"
-                onChange={onChange} fontSize={18} value={editor.userCode} width={"600px"} height={"300px"} />
+                mode="javascript"
+                theme="monokai"
+                onChange={onChange}
+                fontSize={18}
+                value={editor.userCode}
+                width={"600px"}
+                height={"300px"}
+            />
             {/* <ConsoleWrapper onSave={saveButton} onExecute={runButton} onReset={resetButton} console={consoleLog} /> */}
         </div>
-    )
+    );
 }
 
-export default AcEditor
+export default AcEditor;
