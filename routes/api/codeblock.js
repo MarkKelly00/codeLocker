137c429ce0fad23e4f1e75537e8b937e6ef9b05a
Joshua Blank <jmnblnk@yahoo.com>
2020-10-09 21:35:11 -0700

    update schema

--- routes/api/codeblock.js
+++ routes/api/codeblock.js
@@ -4,10 +4,6 @@ const codeController = require("../../controllers/CodeController");
 router.route("/")
     .post(codeController.saveNewCode);
 
-router.route("/health")
-    .get(
-        codeController.retrieveCode
-        // res.status(200).end()
-    )
+
 
 module.exports = router;
