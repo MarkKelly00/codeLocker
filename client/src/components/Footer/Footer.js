615c25739d0446d06f27838a732082874aa69abf
Mako3 <big.mako@gmail.com>
2020-10-10 13:33:41 -0700

    centered isLoading

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() { 
     return (
-        <div className="w-full text-center p-2 pin-b">
+        <div className="w-full text-center p-1 pin-b">
             <footer className="footer text-center text-gray-200">
                 © Copyright { (new Date()).getFullYear() }
             </footer>
