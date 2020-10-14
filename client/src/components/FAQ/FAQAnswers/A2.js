f14076969203a37f1f915b15a016a46cdc503dab
LJFrancis <joe.mengis@gmail.com>
2020-10-13 23:03:14 -0700

    add styling and questions and answers

--- client/src/components/FAQ/FAQAnswers/A2.js
+++ client/src/components/FAQ/FAQAnswers/A2.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const A2 = () => (
-    <p>Answer 2</p>
+    <div>
+        <h3>How to deactivate your account:</h3>
+        <br/>
+        <ul>
+            <li>
+                1. Click on the "profile" icon at the top right of the screen
+            </li>
+            <li>
+                2. From the pop-up menu, click "settings"
+            </li>
+            <li>
+                3. At the bottom of your user profile, click "Deactivate Account"
+            </li>
+        </ul>
+    </div>
 );
 
 export default A2;
--- client/src/components/FAQ/FAQAnswers/A4.js
+++ client/src/components/FAQ/FAQAnswers/A4.js
@@ -1,7 +1,42 @@
 import React from "react";
 
 const A4 = () => (
-    <p>Answer 4</p>
+    <div>
+        <h3>How to edit your profile:</h3>
+        <br/>
+        <ul>
+            <li>
+                1. Click on the "profile" icon at the top right of the screen
+            </li>
+            <li>
+                2. From the pop-up menu, click "settings"
+            </li>
+            <br/>
+            <h3>To change your name:</h3>
+            <br/>
+            <li>
+                3. To change your name, click where it says "List name here" and type in your desired name.
+            </li>
+            <li>
+                4. Click the "Update" button below your name.
+            </li>
+            <li>
+                5. Congrats, your name has been updated!
+            </li>
+            <br/>
+            <h3>To change your email:</h3>
+            <br/>
+            <li>
+                3. To change your email, click where it says "List email here" and type in your desired email address.
+            </li>
+            <li>
+                4. Click the "Update" button below your email.
+            </li>
+            <li>
+                5. Congrats, your email has been updated!
+            </li>
+        </ul>
+    </div>
 );
 
 export default A4;
