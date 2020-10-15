beddd50adf0ed44f91703820357364042ae5f775
LJFrancis <joe.mengis@gmail.com>
2020-10-14 19:19:05 -0700

    remove inputs splash

--- client/src/components/Buttons/LoginButton.js
+++ client/src/components/Buttons/LoginButton.js
@@ -4,7 +4,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <button className="bg-blue-600 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded text-white font-bold shadow sm:shadow-md m-4" onClick={() => loginWithRedirect()}>Log In</button>;
 };
 
 export default LoginButton;
